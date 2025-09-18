a9e6c637e804a961a601dfff00f1ff7e993dcc89
arjun-saini-svg <asphcasee@gmail.com>
2025-09-19 02:10:43 +0530

    working version (could use better and cleaner code)

--- number guessing game/number guessing game/Program.cs
+++ number guessing game/number guessing game/Program.cs
@@ -9,24 +9,37 @@ class Program
 
         int guesses = 0;
 
+        int min = 0;
+
+        int max = 100;
+
+        Random random = new Random();
+
+        int randomNumber = random.Next(min, max + 1);
+
         while (playagain)
         {
-            Console.WriteLine("Guess the number between 0 - 100");
+            Console.WriteLine("Guess the number between 0 - 100:");
 
             int guess = Convert.ToInt32(Console.ReadLine());
-
-            int min = 0;
-
-            int max = 100;
-
-            Random random = new Random();
-
-            int randomNumber = random.Next(min, max + 1);
 
             if (guess == randomNumber)
             {
                 Console.WriteLine("You have guesses the number!");
                 Console.WriteLine($"guesses you took: {guesses}");
+                guesses = 0;
+                Console.WriteLine("Wanna play again?: ");
+                string answer = Console.ReadLine();
+                answer = answer.ToUpper();
+
+                if (answer == "Y" || answer == "YES" || answer == "TRUE")
+                {
+                    playagain = true;
+                }
+                else
+                {
+                    playagain = false;
+                }
             } else if (guess > randomNumber)
             {
                 Console.WriteLine("The number you guessed is too high");
@@ -37,18 +50,6 @@ class Program
 
             guesses++;
 
-            Console.WriteLine("Wanna play again?: ");
-            string answer = Console.ReadLine();
-            answer = answer.ToUpper();
-
-            if (answer == "Y" || answer == "YES" || answer == "TRUE")
-            {
-                playagain = true;
-            }
-            else
-            {
-                playagain = false;
-            }
         }
 
 
