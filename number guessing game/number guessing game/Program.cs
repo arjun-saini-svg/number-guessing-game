43fb672c4914bd0a85bd550c1191b23bfb7afec9
arjun-saini-svg <asphcasee@gmail.com>
2025-09-19 01:25:58 +0530

    basic logic

---
+++ number guessing game/number guessing game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Guess the number between 0 - 100");
+
+        Random random = new Random();
+
+        int min = 0;
+
+        int max = 100;
+
+        random.Next(min, max + 1);
+
+    }
+}
