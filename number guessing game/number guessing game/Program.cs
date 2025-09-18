5fca2b6c4257c2db3443530dc1f09884b3d49dfb
arjun-saini-svg <asphcasee@gmail.com>
2025-09-19 01:47:22 +0530

    addition to basic code

--- number guessing game/number guessing game/Program.cs
+++ number guessing game/number guessing game/Program.cs
@@ -6,13 +6,52 @@ class Program
     {
         Console.WriteLine("Guess the number between 0 - 100");
 
+        int guess = Convert.ToInt32(Console.ReadLine());
+
         Random random = new Random();
 
         int min = 0;
 
         int max = 100;
 
-        random.Next(min, max + 1);
+        int randomNumber = random.Next(min, max + 1);
+
+        bool playagain = true;
+
+        int guesses = 0;
+
+        while (playagain)
+        {
+
+            if(guess == randomNumber)
+            {
+                Console.WriteLine("You have guesses the number!");
+            } else if (guess > randomNumber)
+            {
+                Console.WriteLine("The number you guessed is too high");
+            } else if (guess < randomNumber)
+            {
+                Console.WriteLine("The number you used too low");
+            }
+
+            guesses++;
+            Console.WriteLine($"guesses you took: {guesses}");
+
+            Console.WriteLine("Wanna play again?: ");
+            string answer = Console.ReadLine();
+            answer = answer.ToUpper();
+
+            if (answer == "Y" || answer == "YES" || answer == "TRUE")
+            {
+                playagain = true;
+            }
+            else
+            {
+                playagain = false;
+            }
+        }
+
+
 
     }
 }
